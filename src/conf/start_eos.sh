1d4511eeedd934978644a7d185b8e637c60306ba
Long <long@devopsdragon.com>
2018-07-30 11:58:09 +0530

    updates

---
+++ src/conf/start_eos.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ -f /opt/eosio/bin/data-dir/blocks/blocks.index ]
+then
+    exec /opt/eosio/bin/nodeos --data-dir=/opt/eosio/bin/data-dir --config-dir=/etc/nodeos
+else
+    exec /opt/eosio/bin/nodeos --data-dir=/opt/eosio/bin/data-dir --config-dir=/etc/nodeos --genesis-json=/etc/nodeos-genesis/genesis.json
+fi
