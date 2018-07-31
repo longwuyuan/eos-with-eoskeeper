2b69932d212d3852bb46d7e59574efaf423097f2
Long <long@devopsdragon.com>
2018-07-31 15:37:42 +0530

    reverted to exec format with thos whole string right here

--- src/conf/start_eos.sh
+++
@@ -1,1 +0,0 @@
-/start_eoskeeper.sh ; if [ -f /opt/eosio/bin/data-dir/blocks/blocks.index ]; then exec /opt/eosio/bin/nodeos --data-dir=/opt/eosio/bin/data-dir --config-dir=/etc/nodeos ; else exec /opt/eosio/bin/nodeos --data-dir=/opt/eosio/bin/data-dir --config-dir=/etc/nodeos --genesis-json=/etc/nodeos-genesis/genesis.json; fi
