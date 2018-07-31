92dbad4a317b71ddfb178e58da0fc8edc4feeb14
Long <long@devopsdragon.com>
2018-07-31 23:31:33 +0530

    moved eoskeeper start after nodeos start in /start.sh

--- src/conf/start.sh
+++ src/conf/start.sh
@@ -1,3 +1,2 @@
-/usr/local/bin/eoskeeper.py &
-sleep 5
 if [ -f /opt/eosio/bin/data-dir/blocks/blocks.index ]; then exec /opt/eosio/bin/nodeos --data-dir=/opt/eosio/bin/data-dir --config-dir=/etc/nodeos ; else exec /opt/eosio/bin/nodeos --data-dir=/opt/eosio/bin/data-dir --config-dir=/etc/nodeos --genesis-json=/etc/nodeos-genesis/genesis.json; fi
+/usr/local/bin/eoskeeper.py &
